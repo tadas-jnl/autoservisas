724cc2cea63802c58f9a598f2892eec732617186
tadas-jnl <tjnl@proton.me>
2025-06-17 14:38:28 +0300

    Padaryta front-end pradžia

--- autoservisas/views.py
+++ autoservisas/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Auto, OrderData, Service
 # Create your views here.
 def index(request):
-    return HttpResponse("Labas pasauli!")
+    context = {
+        "cars_count": Auto.objects.all().count(),
+        "paid_orders": OrderData.objects.filter(status__exact='a').count(),
+        "services_count": Service.objects.count(),
+    }
+    return render(template_name="index.html", request=request, context=context)
+
+

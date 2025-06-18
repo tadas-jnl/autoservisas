9e0f8bf2d63bb9158c2348b5c52cdde4332eabf8
tadas-jnl <tjnl@proton.me>
2025-06-18 14:29:46 +0300

    backup

--- autoservisas/urls.py
+++ autoservisas/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    # path('test/', views.test, name='test'),
     path('', views.index, name='index'),
+    path('cars/', views.automobiliai, name='automobiliai'),
+    path('cars/<int:car_id>', views.automobilis, name='automobilis'),
 ]
--- autoservisas/views.py
+++ autoservisas/views.py
@@ -6,7 +6,16 @@ def index(request):
         "cars_count": Auto.objects.all().count(),
         "paid_orders": OrderData.objects.filter(status__exact='a').count(),
         "services_count": Service.objects.count(),
+
     }
     return render(template_name="index.html", request=request, context=context)
 
+def automobiliai(request):
+    context = {
+        "automobiliai": Auto.objects.all(),
+    }
+    return render(request, template_name='cars.html', context=context)
 
+def automobilis(request, car_id):
+    car = Auto.objects.get(pk=car_id)
+    return render(request, template_name='car.html', context={'car': car})
